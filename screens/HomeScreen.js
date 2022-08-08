826bd4c6759347134f29cb996342570caa563bdd
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-08 11:54:50 +0500

    Initial Commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useLayoutEffect } from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { Image, SafeAreaView, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
@@ -14,7 +14,13 @@ const HomeScreen = () => {
   return (
     <SafeAreaView>
       <Text className="text-blue-700 font-bold text-3xl text-center mt-8">
-        HomeScreen
+        {/* Header */}
+        <View>
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
       </Text>
     </SafeAreaView>
   );
